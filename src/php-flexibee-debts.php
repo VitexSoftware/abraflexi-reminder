029821282dcde051e1af104744715bab3662c365
Vítězslav Dvořák <info@vitexsoftware.cz>
2018-05-31 21:44:19 +0200

    Update for new FlexiPeeHP-Bricks

--- src/php-flexibee-debts.php
+++ src/php-flexibee-debts.php
@@ -11,40 +11,46 @@ define('EASE_LOGGER', 'syslog|console|mail');
 
 require_once '../vendor/autoload.php';
 $shared = new Ease\Shared();
-$shared->loadConfig('../client.json');
-$shared->loadConfig('../reminder.json');
+try {
+    $shared->loadConfig('../client.json');
+    $shared->loadConfig('../reminder.json');
 
-$reminder = new \FlexiPeeHP\Bricks\Upominac();
-$reminder->logBanner();
+    $reminder = new \FlexiPeeHP\Bricks\Upominac();
+    $reminder->logBanner();
 
-
-$allDebths = $reminder->getDebths();
-$reminder->addStatusMessage(sprintf(_('%d clients to remind process'),
-        count($allDebths)));
-$counter   = 0;
-$total     = [];
-foreach ($allDebths as $cid => $debts) {
-    $counter++;
-    $howmuchRaw = $howmuch    = [];
-    foreach ($debts as $debt) {
-        $curcode = FlexiPeeHP\FlexiBeeRO::uncode($debt['mena']);
-        if (!isset($howmuchRaw[$curcode])) {
-            $howmuchRaw[$curcode] = 0;
+    $allDebths = $reminder->getDebths(['NEUPOMINKOVAT']);
+    $reminder->addStatusMessage(sprintf(_('%d clients to remind process'),
+            count($allDebths)));
+    $counter   = 0;
+    $total     = [];
+    foreach ($allDebths as $cid => $debts) {
+        $counter++;
+        $howmuchRaw = $howmuch    = [];
+        foreach ($debts as $debt) {
+            $curcode = FlexiPeeHP\FlexiBeeRO::uncode($debt['mena']);
+            if (!isset($howmuchRaw[$curcode])) {
+                $howmuchRaw[$curcode] = 0;
+            }
+            $howmuchRaw[$curcode] += $debt['sumCelkem'];
+            if (!isset($total[$curcode])) $total[$curcode]      = 0;
+            $total[$curcode]      += $debt['sumCelkem'];
         }
-        $howmuchRaw[$curcode] += $debt['sumCelkem'];
-        if (!isset($total[$curcode])) $total[$curcode]      = 0;
-        $total[$curcode]      += $debt['sumCelkem'];
-    }
-    foreach ($howmuchRaw as $cur => $price) {
-        $howmuch[] = $price.' '.$cur;
-    }
+        foreach ($howmuchRaw as $cur => $price) {
+            $howmuch[] = $price.' '.$cur;
+        }
 
-    $reminder->customer->adresar->loadFromFlexiBee($cid);
-    $reminder->addStatusMessage(sprintf('(%d / %d) %s  %s %s [ %s ]', $counter,
-            count($allDebths), implode(',', $howmuch),
-            $reminder->customer->adresar->getDataValue('kod'),
-            $reminder->customer->adresar->getDataValue('nazev'),
-            $reminder->customer->adresar->getDataValue('stitky')
-        ), 'debug');
+        $reminder->customer->adresar->loadFromFlexiBee($cid);
+        $reminder->addStatusMessage(sprintf('(%d / %d) %s  %s %s [ %s ]',
+                $counter, count($allDebths), implode(',', $howmuch),
+                $reminder->customer->adresar->getDataValue('kod'),
+                $reminder->customer->adresar->getDataValue('nazev'),
+                $reminder->customer->adresar->getDataValue('stitky')
+            ), 'debug');
+    }
+    $reminder->addStatusMessage(json_encode($total));
+} catch (Exception $exc) {
+    echo $exc->getMessage()."\n";
+    echo $exc->getTraceAsString();
 }
-$reminder->addStatusMessage(json_encode($total));
+
+
