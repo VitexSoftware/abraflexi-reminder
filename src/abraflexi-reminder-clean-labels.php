c4ca829d7a130eabdf8148a501fabe702e9d7629
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2025-03-13 12:47:14 +0100

    label cleaner now produce json report

--- src/abraflexi-reminder-clean-labels.php
+++ src/abraflexi-reminder-clean-labels.php
@@ -10,11 +10,20 @@
 
 use AbraFlexi\Reminder\Upominac;
 use Ease\Locale;
+use Ease\Shared;
 
 \define('EASE_APPNAME', 'Clean Remind Labels');
 
 require_once '../vendor/autoload.php';
-\Ease\Shared::init(['ABRAFLEXI_URL', 'ABRAFLEXI_LOGIN', 'ABRAFLEXI_PASSWORD', 'ABRAFLEXI_COMPANY'], $argv[1] ?? '../.env');
+$exitcode = 0;
+$options = getopt('o::e::', ['output::environment::']);
+$report = [];
+\Ease\Shared::init(
+    ['ABRAFLEXI_URL', 'ABRAFLEXI_LOGIN', 'ABRAFLEXI_PASSWORD', 'ABRAFLEXI_COMPANY'],
+    \array_key_exists('environment', $options) ? $options['environment'] : (\array_key_exists('e', $options) ? $options['e'] : '../.env'),
+);
+$destination = \array_key_exists('output', $options) ? $options['output'] : Shared::cfg('RESULT_FILE', 'php://stdout');
+
 $localer = new Locale('cs_CZ', '../i18n', 'abraflexi-reminder');
 $reminder = new Upominac();
 
@@ -36,9 +45,10 @@ foreach ($reminder->getCustomerList([implode(' or ', $labelsRequied), 'limit' =>
     $reminder->customer->adresar->setDataValue('stitky', implode(',', $clientInfo['stitky']));
 
     // Check if the customer has no debts
-    if (empty($reminder->customer->getCustomerDebts($clientCode))) {
+    if (empty($reminder->customer->getCustomerDebts(AbraFlexi\Functions::code($clientCode)))) {
         $reminder->customer->adresar->unsetLabel($labelsRequiedRaw);
         $reminder->addStatusMessage(++$pos.'/'.\count($reminder->customer->adresar->lastResult['adresar']).' '.$clientCode.' '._('Labels Cleanup'), ($reminder->customer->adresar->lastResponseCode === 201) ? 'success' : 'warning');
+        $report['removed'][$clientCode] = $labelsRequiedRaw;
     } else {
         $reminder->addStatusMessage($clientCode.' '._('Customer has debts, labels not removed'), 'info');
     }
@@ -47,3 +57,8 @@ foreach ($reminder->getCustomerList([implode(' or ', $labelsRequied), 'limit' =>
 if (!$pos) {
     $reminder->addStatusMessage(_('None to clear'));
 }
+
+$written = file_put_contents($destination, json_encode($report, \Ease\Shared::cfg('DEBUG') ? \JSON_PRETTY_PRINT : 0));
+$reminder->addStatusMessage(sprintf(_('Saving result to %s'), $destination), $written ? 'success' : 'error');
+
+exit($exitcode);
